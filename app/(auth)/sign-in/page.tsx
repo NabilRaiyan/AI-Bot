b2012121aa1c533c65fa88a8d5ba8d72456d2949
NabilRaiyan <rraiyan77@gmail.com>
2025-04-17 21:20:05 +0600

    creating auth forms

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,9 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Sign In
-    </div>
+    <AuthForm type="sign-in" />
   )
 }
 
